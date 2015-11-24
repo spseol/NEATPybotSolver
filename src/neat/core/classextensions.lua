61721543b49694c0c9185ae126fd1136421c6af8
Sony Nguyen <sirarseus@gmail.com>
2015-11-24 19:57:27 +0100

    Fix Lua: property - setter checking

--- src/neat/core/classextensions.lua
+++ src/neat/core/classextensions.lua
@@ -1,16 +1,16 @@
 if not ClassExtensionsDefined then
 
-function property(class, propertyName, getterName, setterName, o, propertyValue, defaultValue)
+function property(class, propertyName, getterName, setterName, object, propertyValue, defaultValue)
 	if type(propertyName) ~= "string" then
 		error("Property name should be <string> got " .. type(propertyName))
 	elseif type(getterName) ~= "string" and getterName ~= nil then
 		error("Getter name should be <string> got " .. type(getterName))
-	elseif type(setterName) ~= "string" and getterName ~= nil then
+	elseif type(setterName) ~= "string" and setterName ~= nil then
 		error("Setter name should be <string> got " .. type(setterName))
 	end
 
-	if o ~= nil then
-		o[propertyName] = propertyValue or (defaultValue or nil)
+	if object ~= nil then
+		object[propertyName] = propertyValue or (defaultValue or nil)
 	end
 
 	if getterName ~= nil then
