ac7d780cf65e7eafbd8ccf4d7d86f9c358b393be
Sony Nguyen <sirarseus@gmail.com>
2015-11-24 19:59:07 +0100

    New Lua: move neat config to neatcore

--- src/neat/neat.lua
+++ src/neat/neat.lua
@@ -1,42 +1,7 @@
+
+
 -----------------------------------------------------------------------------
-Neat = {
-	__innovation = 0,
-	population = 1000,
-
-	IO = {
-		InputsCount = 2,
-		OutputsCount = 1
-	},
-
-	-- settings to determinate genomes compability to divide them into species
-	SpeciesCoefficients = {
-		excess = 1.0,
-		disjoint = 1.0,
-		weight = 0.4,
-		deltaThreshold = 3.0
-	},
-
-	-- mutations chances
-	MutationsChances = {
-		genome = 0.8,
-		weight = 0.9,
-		randomWeight = 0.1
-	},
-
-	-- chance, that property will inherit parent property, crossover chances etc...
-	TansformationChances = {
-		disableIfParentDisable = 0.75,
-		offspringCrossover = 0.75,
-		interspeciesMatingRate = 0.001,
-		newNode = 0.03,
-		newLink = 0.05
-	}
-}
-
-function Neat:_newInnovation()
-	self.__innovation = self.__innovation + 1
-	return self.__innovation
-end
+Neat = {}
 
 function Neat:_checkInnovationInGeneration()
 end
