1fb8c37cfb6244d6ff68da9fa6935ba72326fb31
Sony Nguyen <sirarseus@gmail.com>
2015-11-26 21:58:22 +0100

    Change Lua: move neatcore beneath neat folder

---
+++ src/neat/neatcore.lua
@@ -0,0 +1,44 @@
+NeatCore = {
+    __innovation = 0,
+    population = 1000,
+
+    IO = {
+        InputsCount = 2,
+        OutputsCount = 1,
+        Inputs = {},
+        Outputs = {}
+    },
+
+    -- settings to determinate chromosomes compability to divide them into species
+    SpeciesCoefficients = {
+        excess = 1.0,
+        disjoint = 1.0,
+        weight = 0.4,
+        deltaThreshold = 3.0
+    },
+    
+    -- mutations chances
+    MutationsChances = {
+        chromosome = 0.8,
+        weight = 0.9,
+        randomWeight = 0.1
+    },
+
+    -- chance, that property will inherit parent property, crossover chances etc...
+    TansformationChances = {
+        disableIfParentDisable = 0.75,
+        offspringCrossover = 0.75,
+        interspeciesMatingRate = 0.001,
+        newNode = 0.03,
+        newLink = 0.05
+    }
+}
+
+function NeatCore:newInnovation()
+    self.__innovation = self.__innovation + 1
+    return self.__innovation
+end
+
+-- returns true if innovation already exists in generation
+function NeatCore:checkInnovationinGeneration(innovationGene)
+end
