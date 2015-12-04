f93db969f020e84f6cdc3d1fc86c4bb6f8880e2b
Sony Nguyen <sirarseus@gmail.com>
2015-12-04 20:56:45 +0100

    Change Lua: add math prefix to sigmoid, remove aproximation of returned value

--- src/core/extendedmath.lua
+++ src/core/extendedmath.lua
@@ -1,10 +1,10 @@
-function sigmoid(x)
+function math.sigmoid(x)
     -- round to network could be able to get 1 or -1
-    if x >= 2 then
-        return 1
-    elseif x <= -2 then
-        return -1
-    else 
+    -- if x >= 2 then
+    --     return 1
+    -- elseif x <= -2 then
+    --     return -1
+    -- else 
         return 2 / (1 + math.exp(-4.9 * x)) - 1
-    end
+    -- end
 end
