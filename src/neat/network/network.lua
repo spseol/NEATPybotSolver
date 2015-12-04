7756080d350bebc3bf098f2957a83f5ee46749ea
Sony Nguyen <sirarseus@gmail.com>
2015-12-04 21:07:20 +0100

    Add/Del: remove NetworkLayer, add neuron type

--- src/neat/network/network.lua
+++ src/neat/network/network.lua
@@ -1,7 +1,6 @@
 require "core.classextensions"
 require "neat.network.connection"
 require "neat.network.neuron"
-require "neat.network.networklayer"
 require "neat.neatcore"
 
 Network = {}
@@ -10,42 +9,36 @@ function Network.new(empty)
     local o = {}
     setmetatable(o, { __index = Network })
 
-    property(Network, "__layers", "layers", nil, o, {})
-    -- property(Network, "__neurons", "neurons", "setNeurons", o, {})
+    property(Network, "__neuronsCount", "neuronsCount", nil, o, 0)
+    property(Network, "__neurons", "neurons", nil, o, {})
     property(Network, "__connections", "connections", "setConnections", o, {})
 
     if not empty then
         -- local connections = {}
 
         for i = 1, NeatCore.IO.InputsCount do
-            o:addNeuron(Neuron.new(true), NetworkLayer.input)
+            o:addNeuron(Neuron.new(Neuron.input))
             -- table.insert(o.__neurons.input, Neuron.new(true))
             -- create simples connections, i = input neuron index, Neat.IO.InputsCount + 2 = output neuron index  
             -- table.insert(connections, Connection.new(i, Neat.IO.InputsCount + 2))
         end
         
         -- add one extra for bias
-        o:addNeuron(Neuron.new(true), NetworkLayer.bias)
+        o:addNeuron(Neuron.new(Neuron.bias))
 
         for i = 1, NeatCore.IO.OutputsCount do
-            o:addNeuron(Neuron.new(), NetworkLayer.output)
+            o:addNeuron(Neuron.new(Neuron.output))
+        end
         end
     end
 
     return o
 end
 
-function Network:layerOfNeuron(neuron)
-    for _, layer in pairs(self.__layers) do
-        if layer:hasNeuron(neuron) then
-            return layer:layerMark()
         end
     end
 end
 
-function Network:addNeuron(neuron, layer)
-    if self.__layers[layer] == nil then
-        self.__layers[layer] = NetworkLayer.new(layer)
     end
     
     self.__layers[layer]:addNeuron(neuron)
--- src/neat/network/neuron.lua
+++ src/neat/network/neuron.lua
@@ -1,27 +1,39 @@
 require "core.classextensions"
 
 Neuron = {
-    _ID = 0
+    output = 1,
+    input = 2,
+    bias = 4,
+    hidden = 8,
 }
 
 NeuronMeta = {}
-
 NeuronMeta.__index = Neuron
 function NeuronMeta.__tostring(self)
-    return string.format("Neuron(ID: %i)", self.__ID)
+    local neuronType = {
+        [1] = "Output",
+        [2] = "Input",
+        [4] = "Bias",
+        [8] = "Hidden"
+    }
+    return string.format("%s neuron(ID: %i)", neuronType[self.__type], self.__ID)
 end
 
-function Neuron.new(isInputOrBias)
+function Neuron.new(neuronType, id)
     local o = {}
 
-    if not isInputOrBias then
+    if neuronType ~= Neuron.input then
         property(Neuron, "__inputs", "inputs", "setInputs", o, {})
     end
+    
+    if neuronType ~= Neuron.output then
+        property(Neuron, "__outputs", "outputs", "setOutputs", o, {})
+    end
 
-    property(Neuron, "__isInputOrBias", "isInputOrBias", nil, o, isInputOrBias)
-    property(Neuron, "__ID", "ID", nil, o, Neuron._ID + 1)
-    property(Neuron, "__outputs", "outputs", "setOutputs")
-    Neuron._ID = Neuron._ID + 1
+    property(Neuron, "__inputWeights", "inputWeights", "setInputWeights", o, {})
+    property(Neuron, "__inputNeurons", "inputNeurons", "setInputNeurons", o, {})
+    property(Neuron, "__type", "type", "setType", o, neuronType)
+    property(Neuron, "__ID", "ID", "setID", o, id)
 
     setmetatable(o, NeuronMeta)
 
