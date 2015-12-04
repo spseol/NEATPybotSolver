6efae47afa32e349fa59354d0451afd23813e0b7
Sony Nguyen <sirarseus@gmail.com>
2015-12-04 21:01:08 +0100

    New Lua: NeatCore - Innovation check system

--- src/neat/neatcore.lua
+++ src/neat/neatcore.lua
@@ -34,14 +34,55 @@ NeatCore = {
     Mutation = {
         interspeciesMatingRate = 0.001,
         stepSize = 0.1
-    }
+    },
+    
+    __allInnovations = {}
 }
 
-function NeatCore:newInnovation()
+Innovation = {}
+InnovationMeta = {}
+InnovationMeta.__index = Innovation
+
+function InnovationMeta.__tostring(self)
+    return string.format("Innovation(ID: %i || %i -> %i)", self.__innovation, self.__input, self.__output)
+end
+
+function Innovation.new(input, output, innovationNumber)
+    local o = {}
+    
+    property(Innovation, "__innovation", "innovation", nil, o, innovationNumber)
+    property(Innovation, "__input", "input", nil, o, input)
+    property(Innovation, "__output", "output", nil, o, output)
+    
+    setmetatable(o, InnovationMeta)
+    
+    return o
+end
+
+function NeatCore:getInnovation(innovation)
+    local isNewInnovation = self:checkInnovation(innovation)
+    
+    if isNewInnovation and type(isNewInnovation) == "boolean" then
+        return self:addInnovation(innovation)
+    else
+        return isNewInnovation  -- contains innovation number
+    end
+end
+
+function NeatCore:addInnovation(innovation)
     self.__innovation = self.__innovation + 1
+    self.__allInnovations[self.__innovation] = Innovation.new(innovation:input(), innovation:output(), self.__innovation)
+    
     return self.__innovation
 end
 
--- returns true if innovation already exists in generation
-function NeatCore:checkInnovationinGeneration(innovationGene)
+-- return true if innovation is new return otherwise return innovation number
+function NeatCore:checkInnovation(newInnovation)
+    for innovationNumber, innovation in pairs(self.__allInnovations) do
+        if newInnovation:input() == innovation:input() and newInnovation:output() == innovation:output() then
+            return innovationNumber
+        end
+    end
+    
+    return true
 end
